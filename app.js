7475f565fbc305d9b76c5f9c7fe66aaa283b7c14
Yusuf olatunji  adeshina <72082053+YeezyWest@users.noreply.github.com>
2023-01-10 10:12:33 +0100

    added an eventListener to the signUp button

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-const hamburger = document.querySelector("#hamburger")
-const menu = document.querySelector("#menu")
+const hamburger = document.querySelector("#hamburger");
+const menu = document.querySelector("#menu");
+const signUp = document.querySelectorAll(".sign");
 
 hamburger.addEventListener("click", ()=> {
     menu.classList.toggle("hidden")
+})
+
+signUp.forEach( (sign)=> {
+  sign.addEventListener("click", ()=> {
+    alert("Still working on the signup form...");
+  })
 })
