a2a37b09e1981e6c3feba54c9bd9c1829b11e469
Yusuf olatunji  adeshina <72082053+YeezyWest@users.noreply.github.com>
2023-01-14 21:33:02 +0100

    added an eventListener to a class discount

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const hamburger = document.querySelector("#hamburger");
 const menu = document.querySelector("#menu");
 const signUp = document.querySelectorAll(".sign");
 const learnMore = document.querySelectorAll(".learn");
+const discount = document.querySelectorAll(".discount");
 
 hamburger.addEventListener("click", ()=> {
     menu.classList.toggle("hidden")
@@ -17,4 +18,8 @@ learnMore.forEach( (learn)=> {
   learn.addEventListener("click", ()=> {
     alert("Still working on the learn form...");
   });
+});
+
+discount.addEventListener("click", ()=> {
+  
 });
