61d668bb50d7f1a864976adea6a58b15c3e6f126
Yusuf olatunji  adeshina <72082053+YeezyWest@users.noreply.github.com>
2023-01-17 14:34:23 +0100

    added a drawer

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
-const hamburger = document.querySelector("#hamburger");
-const menu = document.querySelector("#menu");
 const signUp = document.querySelectorAll(".sign");
 const learnMore = document.querySelectorAll(".learn");
 const discount = document.querySelectorAll(".discount");
-
-hamburger.addEventListener("click", ()=> {
-    menu.classList.toggle("hidden")
-})
 
 signUp.forEach( (sign)=> {
   sign.addEventListener("click", ()=> {
